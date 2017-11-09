23dfd4031fd6a73a5eef8cd0763069532ac3916a
pspetrov <pspetrov@it.ru>
2017-11-10 00:11:43 +0300

    initial commit

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace Lesson3
+{
+    class Program
+    {
+        enum Exercises
+        {
+            complex = 1,
+            amount
+
+        }
+
+        struct Complex
+        {
+            public double im;
+            public double re;
+
+            /// <summary>
+            /// метод сложения комплексных чисел
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns></returns>
+            public Complex Plus(Complex x)
+            {
+                Complex temp;
+                temp.im = im + x.im;
+                temp.re = re + x.re;
+                return temp;
+            }
+
+            /// <summary>
+            /// метод вычитания комплексных чисел
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns></returns>
+            public Complex Minus(Complex x)
+            {
+                Complex temp;
+                temp.im = im - x.im;
+                temp.re = re - x.re;
+                return temp;
+            }
+
+            /// <summary>
+            /// метод умножения комплексных чисел
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns></returns>
+            public Complex Multi(Complex x)
+            {
+                Complex temp;
+                temp.im = im * x.im + re * x.im;
+                temp.re = re * x.im - im * x.re;
+                return temp;
+            }
+
+            /// <summary>
+            /// метод деления комплексных чисел
+            /// </summary>
+            /// <param name="x"></param>
+            /// <returns></returns>
+            public Complex Div(Complex x)
+            {
+                Complex temp;
+                temp.re = (re * x.re + im * x.im) / (x.re * x.re + x.im * x.im);
+                temp.im = (x.re * im - re * x.im) / (x.re * x.re + x.im * x.im);
+                return temp;
+            }
+
+
+            public string ToString()
+            {
+                return re + "+" + im + "i";
+            }
+        }
+
+        /// <summary>
+        /// Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+        /// </summary>
+        static void Ex1()
+        {
+            Complex complex1;
+            complex1.re = 3;
+            complex1.im = 6;
+            Complex complex2;
+            complex2.re = 1;
+            complex2.im = 2;
+            Console.WriteLine($"Комплексное число 1 = {complex1.re}+{complex1.im}i");
+            Console.WriteLine($"\nКомплексное число 2 = {complex2.re}+{complex2.im}i");
+            Complex result = complex1.Plus(complex2);
+            Console.WriteLine($"\nРезультат сложения: " + result.ToString());
+            result = complex1.Multi(complex2);
+            Console.WriteLine($"\nРезультат Умножения: " + result.ToString());
+            result = complex1.Minus(complex2);
+            Console.WriteLine($"\nРезультат вычитания: " + result.ToString());
+            result = complex1.Div(complex2);
+            Console.WriteLine($"\nРезультат деления: " + result.ToString());
+        }
+
+        /// <summary>
+        /// Требуется подсчитать сумму всех нечетных положительных чисел. Сами числа и сумму вывести на экран;
+        /// Используя tryParse; 
+        /// б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные.
+        /// При возникновении ошибки вывести сообщение.
+        /// </summary>
+        static void Ex2()
+        {
+
+            int a = Utils.GetValue();
+
+            var sum = 0;
+            while (a != 0)
+            {
+
+                if ((a > 0) && (a % 2 != 0))
+                {
+                    sum = sum + a;
+                }
+                a = Utils.GetValue();
+            }
+            Console.Write($"\nСумма всех нечетных положительных чисел = {sum}");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void Ex3()
+        {
+
+        }
+
+
+
+        /// <summary>
+        ///Инструменты расположены в методах класса Utils, тут производится вызов этих методов.
+        /// </summary>
+        static void Main()
+        {
+            Console.Write("Введите номер задания для проверки(1-3): ");
+            Exercises Ex = (Exercises)Convert.ToInt32(Console.ReadLine());
+            switch (Ex)
+            {
+
+                case Exercises.complex: Ex1(); break;
+                case Exercises.amount: Ex2(); break;
+                //case Exercises.amount: Ex3(); break;
+
+                default: Console.Write($"нет такого задания! \nВведите еще раз.. "); Main(); break;
+            }
+
+            //Завершение...
+            Console.Write($"\nДля выбора задания введите b, для выхода любое другое значения:");
+            if (Convert.ToString(Console.ReadLine()).ToLower() == "b") { Main(); }
+        }
+
+    }
+}
---
+++ Lesson3/Lesson3/Utils.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    public class Utils
+    {
+
+
+        /// <summary>
+        /// Написать метод возвращающий минимальное из трех чисел.
+        /// </summary>
+        public static int Minimum(int a, int b, int c)
+        {
+            var max = (a < b && a < c) ? a : (b < c) ? b : c;
+            return max;
+        }
+
+        /// <summary>
+        /// Метод подсчета длины значения
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static int Quantity(int a)
+        {
+            var q = Convert.ToString(a).Length;
+            return q;
+        }
+
+
+
+        /// <summary>
+        /// Метод подсчета суммы всех нечетных положительных чисел.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static int GetValue()
+        {
+            int x;
+            string s;
+            bool flag; 
+            do
+            {
+                Console.WriteLine("Введите значение");
+                s = Console.ReadLine();
+                flag = int.TryParse(s, out x);
+                if (!flag) Console.WriteLine("Введенное значение не является целым числом!");
+            }
+            while (!flag); 
+            return x;
+        }
+
+        /// <summary>
+        ///Метод проверки логина и пароля
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static bool PassCheck(string login, string pass)
+        {
+            string log = "root";
+            string password = "GeekBrains";
+            bool result = false;
+            if (login.ToLower() == log)
+            {
+                if (pass == password)
+                {
+                    result = true;
+                }
+            }
+            else { result = false; }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Подсчет индекса массы тела
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="weight"></param>
+        /// <returns></returns>
+        public static double IMT(Double height, Double weight)
+        {
+            double imt = weight / Math.Pow(height, 2);
+            return imt;
+        }
+
+        /// <summary>
+        /// Метод подсчета суммы цифр числа
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static int summ(int a)
+        {
+            var sum = 0;
+            while (a > 0)
+            {
+                sum += a % 10;
+                a /= 10;
+            }
+            return sum;
+        }
+
+        public static void Loop(int a, int b)
+        {
+            Console.Write($"\n{a}");
+            if (a < b) Loop(a + 1, b);
+        }
+
+        /// <summary>
+        /// Рекурсивный метод подсчета суммы
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        public static long LoopSumm(int a, int b)
+        {
+
+            if (a < b) { return a + LoopSumm(a + 1, b); }
+            else { return a; }
+        }
+
+
+    }
+}
